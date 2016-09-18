b18170edac2622cf01f9c95cfeb331fc6ffc076c
wnamen <williamjosephnamen@gmail.com>
2016-09-17 20:03:06 -0700

    built structure

---
+++ app.js
@@ -0,0 +1,27 @@
+angular.module('bookApp', ['ngRoute'])
+       .config(config);
+
+////////////
+// ROUTES //
+////////////
+
+config.$inject = ['$routeProvider', '$locationProvider'];
+function config (  $routeProvider,   $locationProvider  )  {
+  $routeProvider
+    .when('/', {
+      templateUrl: /* Include the path to the index template */,
+      controller:  /* Which controller do you want the main page to use */,
+      controllerAs:/* What will you call the controller in the html? */
+    })
+    /* Include the additional route here! */
+    .otherwise({
+      redirectTo: '/'
+    });
+
+  // this just makes it so our URLs don't have /#/ in them.
+  $locationProvider
+    .html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+};
---
+++ controllers/BooksShowController.js
@@ -0,0 +1,8 @@
+
+angular.module('libraryApp')
+  .controller('BooksIndexController', BooksIndexController);
+
+BooksShowController.$inject=['$http', '$routeParams', '$location'];
+function BooksShowController($http, $routeParams, $location) {
+  var vm = this;
+};
